f89a9f44385fafe6c8fd91a35a0e3f28b6faaef8
茹凌丰 <512387589@qq.com>
2022-08-29 09:11:09 +0800

    CompletableFuture function工具类

--- rlf-module/src/main/java/com/rlf/module/threadDispatch/CompletableFutureBlockUtils.java
+++ rlf-module/src/main/java/com/rlf/module/threadDispatch/CompletableFutureBlockUtils.java
@@ -3,7 +3,10 @@ package com.rlf.module.threadDispatch;
 import cn.hutool.core.collection.CollectionUtil;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.rlf.module.entity.Car;
+import com.rlf.module.entity.User;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 import java.util.List;
 import java.util.concurrent.*;
@@ -31,50 +34,70 @@ public class CompletableFutureBlockUtils {
 
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+
+
+//        List<Integer> userList2 = Lists.newArrayList(1,2,3);
+//        List<Integer> resList = asyncHandleListAndSyncReturn(userList2, (val) -> {
+//            //异步处理逻辑
+//                CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+//
+//                    System.out.println(val);
+//                    Integer res = val;
+//                    return res;
+//                }, threadPoolExecutor);
+//                return future;
+//            }
+//        );
+
+//        List<CompletableFuture<Integer>> collect = userList.stream().map(i -> {
+//            return CompletableFuture.supplyAsync(() -> {
+//                System.out.println(i);
+//                Integer res = i;
+//                return res;
+//            }, threadPoolExecutor);
+//
+//        }).collect(toList());
+
+
+
         //原数据
-        List<Integer> userList = Lists.newArrayList(1, 2, 3);
+        List<User> userList = Lists.newArrayList(new User("名称1"),new User("名称2"),new User("名称3"));
+        List<CompletableFuture<Car>> collect1 = userList.stream().map(i -> hanleFunction(i, (val) -> CompletableFuture.supplyAsync(() -> {
+
+            System.out.println(val.getUserName());
+            Car car = new Car();
+            car.setModel(val.getUserName());
+            return car;
+        }, threadPoolExecutor))).collect(toList());
 
 
-        List<Integer> resList = asyncHandleListAndSyncReturn(userList, (val) -> {
-            //异步处理逻辑
-                CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
-                    try {
-                        TimeUnit.SECONDS.sleep(3);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(val);
-                    Integer res = val;
-                    return res;
-                }, threadPoolExecutor);
-                return future;
-            }
-        );
-        System.out.println(resList);
+        List<Car> integers = blockThreadAndGet(collect1);
+        System.out.println(integers);
     }
 
 
-    /**
-     * 实现方式CompletableFuture.supplyAsync,并且同步返回结果
-     * @param com
-     * @param function
-     * @param <T>
-     * @param <F>
-     * @return
-     * @throws ExecutionException
-     * @throws InterruptedException
-     */
-    public static <T,F> List<T> asyncHandleListAndSyncReturn(List<T> com,Function<T,F> function) throws ExecutionException, InterruptedException {
-        if(CollectionUtil.isEmpty(com)){
-            return null;
-        }
-        List<CompletableFuture<F>> collect = com.stream().map(i -> hanleFunction(i, function)).collect(toList());
-        return (List<T>) blockThreadAndGet(collect);
+//    /**
+//     * 实现方式CompletableFuture.supplyAsync,并且同步返回结果
+//     * @param com
+//     * @param function
+//     * @param <T>
+//     * @param <F>
+//     * @return
+//     * @throws ExecutionException
+//     * @throws InterruptedException
+//     */
+//    public static <T,F> List<T> asyncHandleListAndSyncReturn(List<T> com,Function<T,F> function) throws ExecutionException, InterruptedException {
+//        if(CollectionUtil.isEmpty(com)){
+//            return null;
+//        }
+//        List<CompletableFuture<F>> collect = com.stream().map(i -> hanleFunction(i, function)).collect(toList());
+//        return (List<T>) blockThreadAndGet(collect);
+//
+//    }
 
-    }
-
-    public static <T,R>CompletableFuture<R> hanleFunction(T value, Function<T,R> function)  {
-        return (CompletableFuture<R>) function.apply(value);
+    public static <T,R> CompletableFuture<R> hanleFunction(T value, Function<T,CompletableFuture<R>> function)  {
+        return function.apply(value);
     }
 
 
@@ -85,6 +108,15 @@ public class CompletableFutureBlockUtils {
         }
         return resList;
     }
+
+
+//    public static <F> List<F> blockThreadAndGet(List<F> completableFutureList) throws ExecutionException, InterruptedException {
+//        List<F> resList = Lists.newArrayList();
+//        for (CompletableFuture<F> future : completableFutureList) {
+//            resList.add(future.get());
+//        }
+//        return resList;
+//    }
 
 
 
